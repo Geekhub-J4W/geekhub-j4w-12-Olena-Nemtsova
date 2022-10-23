e1f3c7ec020a8de76892f6ef9b08f7b4c0c8159c
Olena-Nemtsova <assassin1me1mow@gmail.com>
2022-10-23 18:27:46 +0300

    "L01 google_checks.xml has been added + .editorconfig"

--- Homework/java-core/01-Introduction/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/01-Introduction/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -2,6 +2,6 @@ package edu.geekhub.homework;
 
 public class ApplicationStarter {
     public static void main(String[] args) {
-       System.out.println("Hello world!");
+        System.out.println("Hello world!");
     }
 }
