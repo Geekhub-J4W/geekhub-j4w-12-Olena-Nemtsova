06676ca3a11336f65fd0fe31b16f87f9eddbae80
Olena-Nemtsova <assassin1me1mow@gmail.com>
2022-10-27 22:59:11 +0300

    "L02 Created calculate program"

--- Homework/java-core/02-Basics/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/02-Basics/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,12 +1,29 @@
 package edu.geekhub.homework;
 
+
+import java.util.Scanner;
+
 public class ApplicationStarter {
     public static void main(String[] args) {
-        double calculated = calculate(12);
+        Scanner s = new Scanner(System.in);
+        String text;
+        while(true) {
+            System.out.println("Enter int number:");
+            text = s.next();
+            try {
+                int n = Integer.parseInt(text);
+                System.out.printf("Result: %.2f %n", calculate(n));
+                s.close();
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Error! "+e.getMessage());
+            }
+        }
     }
 
     private static double calculate(int n) {
-        // Write code here :)
-        return 0;
+        if (n % 2 == 0) return Math.pow(n, 2);
+        else if (n % 3 == 0) return Math.PI * Math.pow(n, 2);
+        else return Math.pow(n, 2) * Math.sqrt(3) / 4;
     }
 }
