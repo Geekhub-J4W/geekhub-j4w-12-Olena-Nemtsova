f41a7a80e493ddbe693cef2779df8887258c3048
Olena-Nemtsova <assassin1me1mow@gmail.com>
2022-11-11 23:48:19 +0200

    "L04 modified 2 classes: UserController, UserService + added exception class UserDataFailedException"

--- Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/UserService.java
+++ Homework/java-core/04-Exceptions/src/main/java/edu/geekhub/homework/UserService.java
@@ -1,12 +1,68 @@
 package edu.geekhub.homework;
 
+import edu.geekhub.exceptions.ConnectionInterruptedException;
+import edu.geekhub.models.User;
 import edu.geekhub.storage.Repository;
 import edu.geekhub.storage.MemoryStorage;
+
+import java.util.UUID;
 
 // Don't move this class
 public class UserService {
 
     private final Repository repository = new MemoryStorage();
+    private User[] users = new User[0];
 
-    //ToDo Write homework-related code here
+    public void addUser(User user) {
+        boolean isAdd = false;
+        while (!isAdd) {
+            try {
+                repository.tryToAdd(user);
+                setUsers();
+                isAdd = true;
+            } catch (ConnectionInterruptedException e) {
+            }
+        }
+    }
+
+    public void setUsers() {
+        while (true) {
+            try {
+                users = repository.tryToGetAll();
+                break;
+            } catch (ConnectionInterruptedException e) {
+            }
+        }
+    }
+
+    public User[] getUsers() {
+        return users;
+    }
+
+    public boolean containEmail(String email) {
+        if (users.length > 0) {
+            for (User item : users) {
+                if (item.getEmail().equals(email)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containId(UUID id) {
+        if (users.length > 0) {
+            for (User item : users) {
+                if (item.getId().equals(id)) return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean containUserName(String userName) {
+        if (users.length > 0) {
+            for (User item : users) {
+                if (item.getUserName().equals(userName)) return true;
+            }
+        }
+        return false;
+    }
 }
