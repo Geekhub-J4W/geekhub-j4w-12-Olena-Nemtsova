045193893b43747fc9c30a7e550f5e6ff2da81f6
Olena-Nemtsova <assassin1me1mow@gmail.com>
2022-12-12 02:09:34 +0200

    "L08 done homework: written missing code + add tests"

--- Homework/java-core/08-Stream-Api/src/main/java/edu/geekhub/homework/Exercises.java
+++ Homework/java-core/08-Stream-Api/src/main/java/edu/geekhub/homework/Exercises.java
@@ -1,52 +1,117 @@
 package edu.geekhub.homework;
 
-import java.util.Map;
+import java.util.*;
+
 import java.util.stream.Collectors;
 
 public class Exercises {
     private static final Cities citiesRepo = new Cities();
 
     public Map<String, Long> getCountryCitiesCount() {
-        // Find the number of cities of each country (use grouping)
         var cities = citiesRepo.getAllCities();
+
         return cities.values().stream()
-                .collect(Collectors.groupingBy(City::getCountryCode,
-                                Collectors.counting()
-                        )
-                );
+            .collect(Collectors.groupingBy((City city) -> {
+                        city = validateCity(city);
+                        return Optional.ofNullable(city.getCountryCode()).orElse("Unknown");
+                    },
+                    Collectors.counting()
+                )
+            );
+    }
+
+    private City validateCity(City city) {
+        return Optional.ofNullable(city)
+            .orElseThrow(() -> new IllegalArgumentException("City was null"));
     }
 
     public City mostPopulatedCity() {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+            .max(Comparator.comparing((City city) -> {
+                        city = validateCity(city);
+                        return city.getPopulation();
+                    }
+                )
+            )
+            .orElseThrow(() -> new IllegalArgumentException("Most populated city cannot be found"));
     }
 
     public City minPopulatedCity() {
-        // Find the min populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+            .min(Comparator.comparing((City city) -> {
+                        city = validateCity(city);
+                        return city.getPopulation();
+                    }
+                )
+            )
+            .orElseThrow(() -> new IllegalArgumentException("Min populated city cannot be found"));
     }
 
     public String mostPopulatedCountry() {
-        // Find the most populated city
+        Map.Entry<String, Integer> entry = populationOfEachCountry()
+            .entrySet().stream()
+            .max(Map.Entry.comparingByValue())
+            .orElseThrow(() -> new IllegalArgumentException("Most populated country cannot be found"));
+
+        return entry.getKey();
     }
 
     public String minPopulatedCountry() {
-        // Find the most populated city
+        Map.Entry<String, Integer> entry = populationOfEachCountry()
+            .entrySet().stream()
+            .min(Map.Entry.comparingByValue())
+            .orElseThrow(() -> new IllegalArgumentException("Min populated country cannot be found"));
+
+        return entry.getKey();
     }
 
     public Long totalPopulation() {
-        // Find the most populated city
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+            .mapToLong((City city) -> {
+                    city = validateCity(city);
+                    return city.getPopulation();
+                }
+            )
+            .sum();
     }
 
-    public Map<String,Integer> populationOfEachCountry() {
-        // Find the most populated city
+    public Map<String, Integer> populationOfEachCountry() {
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+            .collect(Collectors.groupingBy((City city) -> {
+                        city = validateCity(city);
+                        return Optional.ofNullable(city.getCountryCode()).orElse("Unknown");
+                    },
+                    Collectors.summingInt(City::getPopulation)
+                )
+            );
     }
 
     public Integer populationOfSpecificCountry(String countryCode) {
-        // Find the most populated city
+        Map<String, Integer> populationOfAll = populationOfEachCountry();
+
+        boolean isCountryPresent = populationOfAll.keySet().stream()
+            .anyMatch(key -> key.equals(countryCode));
+        if (isCountryPresent) {
+            return populationOfAll.get(countryCode);
+        } else {
+            throw new IllegalArgumentException("Nothing found for country code: " + countryCode);
+        }
     }
 
-    public City specificCy(String city) {
-        // Find the most populated city
+    public City specificCity(String city) {
+        var cities = citiesRepo.getAllCities();
+
+        return cities.values().stream()
+            .filter(o -> o.getName().equals(city))
+            .findFirst()
+            .orElseThrow(() -> new IllegalArgumentException(city + " city wasn't found"));
     }
-
-
 }
---
+++ Homework/java-core/08-Stream-Api/src/test/java/edu/geekhub/homework/ExercisesTest.java
@@ -0,0 +1,197 @@
+package edu.geekhub.homework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExercisesTest {
+    private Exercises exercises;
+    Map<Integer, City> cities;
+
+    @BeforeEach
+    void setUp() {
+        exercises = new Exercises();
+        cities = new Cities().getAllCities();
+    }
+
+    @Test
+    void can_get_most_populated_city() {
+        City city = exercises.mostPopulatedCity();
+
+        int max_population = 0;
+        int max_i = 1;
+        for (Map.Entry<Integer, City> entry : cities.entrySet()) {
+            int i = entry.getKey();
+            int population = cities.get(i).getPopulation();
+            if (population > max_population) {
+                max_population = population;
+                max_i = i;
+            }
+        }
+
+        assertEquals(cities.get(max_i), city);
+    }
+
+    @Test
+    void can_get_min_populated_city() {
+        City city = exercises.minPopulatedCity();
+
+        int min_i = 1;
+        int min_population = cities.get(min_i).getPopulation();
+        for (Map.Entry<Integer, City> entry : cities.entrySet()) {
+            int i = entry.getKey();
+            int population = cities.get(i).getPopulation();
+            if (population < min_population) {
+                min_population = population;
+                min_i = i;
+            }
+        }
+
+        assertEquals(cities.get(min_i), city);
+    }
+
+    @Test
+    void can_get_max_populated_country() {
+        String country = exercises.mostPopulatedCountry();
+
+        Map<String, Integer> countriesPopulation = getPopulationForEachCountry();
+        String expectedCountry = null;
+        for (String it : countriesPopulation.keySet()) {
+            if (expectedCountry == null || countriesPopulation.get(it) > countriesPopulation.get(expectedCountry)) {
+                expectedCountry = it;
+            }
+        }
+
+        assertEquals(expectedCountry, country);
+    }
+
+    @Test
+    void can_get_min_populated_country() {
+        String country = exercises.minPopulatedCountry();
+
+        Map<String, Integer> countriesPopulation = getPopulationForEachCountry();
+        String expectedCountry = null;
+        for (String it : countriesPopulation.keySet()) {
+            if (expectedCountry == null || countriesPopulation.get(it) < countriesPopulation.get(expectedCountry)) {
+                expectedCountry = it;
+            }
+        }
+
+        assertEquals(expectedCountry, country);
+    }
+
+    @Test
+    void can_get_total_populated() {
+        Long sum = exercises.totalPopulation();
+
+        Long expectedSum = 0L;
+        for (Map.Entry<Integer, City> entry : cities.entrySet()) {
+            int i = entry.getKey();
+            expectedSum += cities.get(i).getPopulation();
+        }
+
+        assertEquals(expectedSum, sum);
+    }
+
+    @Test
+    void can_get_population_each_country() {
+        Map<String, Integer> countriesPopulation = exercises.populationOfEachCountry();
+
+        Map<String, Integer> expectedCountriesPopulation = getPopulationForEachCountry();
+
+        assertEquals(expectedCountriesPopulation, countriesPopulation);
+    }
+
+    @Test
+    void can_get_specific_city() {
+        String cityName = "Wollongong";
+        City city = exercises.specificCity(cityName);
+
+        City expectedCity = null;
+        for (Map.Entry<Integer, City> entry : cities.entrySet()) {
+            int i = entry.getKey();
+            if (cities.get(i).getName().equals(cityName)) {
+                expectedCity = cities.get(i);
+                break;
+            }
+        }
+
+        assertEquals(expectedCity, city);
+    }
+
+    @Test
+    void can_not_get_wrong_specific_city() {
+        String cityName = "Non-existent city";
+        IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> exercises.specificCity(cityName)
+        );
+
+        assertEquals(cityName + " city wasn't found", thrown.getMessage());
+    }
+
+    @Test
+    void can_get_population_of_specific_country() {
+        String countryCode = "AUS";
+        Integer populationOfCountry = exercises.populationOfSpecificCountry(countryCode);
+
+        Integer expectedPopulationOfCountry = 0;
+        for (Map.Entry<Integer, City> entry : cities.entrySet()) {
+            int i = entry.getKey();
+            if (cities.get(i).getCountryCode().equals(countryCode)) {
+                expectedPopulationOfCountry += cities.get(i).getPopulation();
+            }
+        }
+
+        assertEquals(expectedPopulationOfCountry, populationOfCountry);
+    }
+
+    @Test
+    void can_not_get_population_of_wrong_specific_country() {
+        String countryCode = "Non-existent country";
+        IllegalArgumentException thrown = assertThrows(
+            IllegalArgumentException.class,
+            () -> exercises.populationOfSpecificCountry(countryCode)
+        );
+
+        assertEquals("Nothing found for country code: " + countryCode, thrown.getMessage());
+    }
+
+    @Test
+    void can_get_country_cities_count() {
+        Map<String, Long> countryCitiesCount = exercises.getCountryCitiesCount();
+
+        Map<String, Long> expectedCountryCitiesCount = new HashMap<>();
+        for (Map.Entry<Integer, City> entry : cities.entrySet()) {
+            int i = entry.getKey();
+            String key = cities.get(i).getCountryCode();
+            if (expectedCountryCitiesCount.containsKey(key)) {
+                expectedCountryCitiesCount.put(key, expectedCountryCitiesCount.get(key) + 1);
+            } else {
+                expectedCountryCitiesCount.put(key, 1L);
+            }
+        }
+
+        assertEquals(expectedCountryCitiesCount, countryCitiesCount);
+    }
+
+    Map<String, Integer> getPopulationForEachCountry() {
+        Map<String, Integer> countriesPopulation = new HashMap<>();
+
+        for (Map.Entry<Integer, City> entry : cities.entrySet()) {
+            int i = entry.getKey();
+            String key = cities.get(i).getCountryCode();
+            if (countriesPopulation.containsKey(key)) {
+                countriesPopulation.put(key, countriesPopulation.get(key) + cities.get(i).getPopulation());
+            } else {
+                countriesPopulation.put(key, cities.get(i).getPopulation());
+            }
+        }
+
+        return countriesPopulation;
+    }
+}
