9f2ea5f2de5f66f4a2c8872663463734459bd337
Olena-Nemtsova <assassin1me1mow@gmail.com>
2022-12-26 04:10:24 +0200

    "L10: written menu at ApplicationStarter"

--- Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
+++ Homework/java-core/10-IO-NIO/src/main/java/edu/geekhub/homework/ApplicationStarter.java
@@ -1,6 +1,60 @@
 package edu.geekhub.homework;
 
+import edu.geekhub.homework.downloading.SongDownloader;
+import edu.geekhub.homework.logging.MyLogger;
+
+import java.util.Scanner;
+
 public class ApplicationStarter {
 
-    // Write code here :)
+    public static void main(String[] args) {
+        SongDownloader downloader = new SongDownloader();
+        downloader.downloadAll();
+
+        MyLogger.saveToFile();
+
+        showMenu();
+    }
+
+    static void showMenu() {
+        int choice = getChoice("Show logs:\n1.All\n2.ERROR\n3.INFO");
+        switch (choice) {
+            case 1 -> {
+                choice = getChoice("Order by:\n1.Date\n2.Log level");
+                if (choice == 1) {
+                    MyLogger.sortByDate();
+                } else {
+                    MyLogger.sortByLevel();
+                }
+                MyLogger.getAll().forEach(ApplicationStarter::print);
+            }
+            case 2 -> MyLogger.getOnlyERROR().forEach(ApplicationStarter::print);
+            default -> MyLogger.getOnlyINFO().forEach(ApplicationStarter::print);
+        }
+    }
+
+    static int getChoice(String message) {
+        int choicesCount = (int) message.lines().count() - 1;
+        while (true) {
+            Scanner scanner = new Scanner(System.in);
+            print(message);
+            try {
+                if (!scanner.hasNextInt()) {
+                    throw new IllegalArgumentException("Error! Incorrectly entered choice!");
+                }
+
+                int choice = scanner.nextInt();
+                if (choice > choicesCount || choice < 0) {
+                    throw new IllegalArgumentException("Error! Incorrectly entered choice!");
+                }
+                return choice;
+            } catch (IllegalArgumentException ex) {
+                print(ex.getMessage());
+            }
+        }
+    }
+
+    static void print(Object objectToPrint) {
+        System.out.println(objectToPrint);
+    }
 }
